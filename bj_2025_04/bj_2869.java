a0884d5e6e46efd79fd06e1b6524a3d4f849cc9e
YunhuiHwang <110009116+YunhuiHwang@users.noreply.github.com>
2025-04-06 22:32:38 +0900

    Add files via upload

---
+++ bj_2025_04/bj_2869.java
@@ -0,0 +1,22 @@
+package bj_2025_04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int high = (V - B) / (A - B);
+        if ((V - B) % (A - B) != 0) high++;
+        System.out.println(high);
+        br.close();
+    }
+}
