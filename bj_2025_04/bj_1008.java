4a1c288614252416da9fbad20798cee6d50ad76b
YunhuiHwang <110009116+YunhuiHwang@users.noreply.github.com>
2025-04-15 23:45:04 +0900

    Add files via upload

--- bj_2025_04/bj_1008.java
+++ bj_2025_04/bj_1008.java
@@ -1,4 +1,19 @@
 package bj_2025_04;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class bj_1008 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        double a = Double.parseDouble(input[0]);
+        double b = Double.parseDouble(input[1]);
+
+        System.out.println(a / b);
+
+        br.close();
+    }
 }
