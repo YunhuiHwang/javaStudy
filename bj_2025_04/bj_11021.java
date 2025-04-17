a08c1a05d8f55b217b100e8a5b9cc56a7d16a4a7
YunhuiHwang <110009116+YunhuiHwang@users.noreply.github.com>
2025-04-18 00:28:23 +0900

    Add files via upload

---
+++ bj_2025_04/bj_11021.java
@@ -0,0 +1,26 @@
+package bj_2025_04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append("Case #"+i+": "+ (a + b)).append("\n");
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+}
+
